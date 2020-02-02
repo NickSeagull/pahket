7e9dde4cb4b1939a9e740684dccb539f6227b555
Nikita Tchayka <nick@booster.cloud>
2020-02-02 01:25:30 +0000

    🚧 Allow passing file

--- src/Pahket.hs
+++ src/Pahket.hs
@@ -8,10 +8,13 @@ import Network.HTTP.Types.Status
 import Network.Wai.Handler.Warp (defaultSettings, setPort)
 import qualified Pahket.AHK as AHK
 import Pahket.Core
+import System.Environment (getArgs)
 import Web.Scotty as HTTP
 
 run :: IO ()
 run = do
+  (filename : _) <- getArgs
+  contents <- readFileText filename
   finishIt <- newEmptyMVar
   t <- forkIO $ scottyOpts (def {verbose = 0, settings = setPort 3000 defaultSettings}) $ do
     HTTP.post "/log" $ do
@@ -28,4 +31,4 @@ run = do
     _ <- readMVar finishIt
     killThread t
     exitSuccess
-  runApp simpleEnv $ AHK.run AHK.exampleProgram
+  runApp simpleEnv $ AHK.run contents

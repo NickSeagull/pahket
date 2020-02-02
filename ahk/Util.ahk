8a64feacc075395fb18ff09f060fb2ad2b1b3c02
Nikita Tchayka <nick@booster.cloud>
2020-02-02 01:16:18 +0000

    🚧 Allow killing server

--- ahk/Util.ahk
+++ ahk/Util.ahk
@@ -20,7 +20,26 @@ print(msg)
   ToolTip, % msg
 }
 
+pahket_exit_server()
+{
+  ; TODO: Load port from env var
+  BaseURL := "http://localhost:3000"
+  Http := ComObjCreate("WinHTTP.WinHTTPRequest.5.1")
+  ; Send the request
+  Http.Open("GET", BaseURL . "/end", False)
+  Http.Send()
+
+  ; Request was unsuccessful
+  if (Http.status != 200 && Http.status != 204)
+  {
+    throw Exception("Request failed: " Http.status
+      ,, Method " " Endpoint "`n" Http.responseText)
+  }
+  ToolTip, % msg
+}
+
 print("hello")
 print("hello1")
 print("hello2")
 print("hello3")
+pahket_exit_server()
--- src/Pahket.hs
+++ src/Pahket.hs
@@ -5,16 +5,26 @@ where
 
 -- import Pahket.Core
 
-import Data.Aeson (FromJSON (..))
+import Control.Concurrent (forkIO, killThread)
 import Network.HTTP.Types.Status
 import Network.Wai.Handler.Warp (defaultSettings, setPort)
 import Web.Scotty as HTTP
 
 run :: IO ()
-run = scottyOpts (def {verbose = 0, settings = setPort 3000 defaultSettings})
-  $ HTTP.post "/log"
-  $ do
-    logMessage <- jsonData
-    putTextLn logMessage
-    status status200
-    text ""
+run = do
+  finishIt <- newEmptyMVar
+  t <- forkIO $ scottyOpts (def {verbose = 0, settings = setPort 3000 defaultSettings}) $ do
+    HTTP.post "/log" $ do
+      logMessage <- jsonData
+      putTextLn logMessage
+      status status200
+      text ""
+    HTTP.get "/end" $ do
+      putTextLn "bye"
+      status status200
+      putMVar finishIt ()
+      text ""
+  forever $ do
+    x <- readMVar finishIt
+    killThread t
+    exitSuccess

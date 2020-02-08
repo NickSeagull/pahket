cf68867458415e046c9c364b8c4b5440117ed09a
Nikita Tchayka <nick@booster.cloud>
2020-02-08 16:56:47 +0000

    🔥 Remove unused stuff

--- cli/src/Pahket.hs
+++ cli/src/Pahket.hs
@@ -3,12 +3,6 @@ module Pahket
   )
 where
 
--- import Control.Concurrent (forkIO)
--- import qualified Pahket.AHK as AHK
--- import Pahket.Core
--- import qualified Pahket.Core.Init as Init
--- import qualified Pahket.Finisher as Finisher
--- import qualified Pahket.Server as Server
 import qualified Pahket.Commands.Help as Help
 import qualified Pahket.Commands.Install as Install
 import qualified Pahket.Commands.Run as Run
@@ -19,17 +13,3 @@ run ("version" : _) = Version.run
 run ("install" : _) = Install.run
 run ("run" : args) = Run.run args
 run _ = Help.run
--- env <- Init.env
--- runWithEnv env
-
--- runWithEnv :: Env App -> IO ()
--- runWithEnv env = do
---   serverThreadId <- forkIO $ runApp env $ do
---     logDebug "Starting server"
---     _ <- liftIO $ Server.run env
---     logDebug "Running AHK app"
---     AHK.run
---   runApp env $ do
---     logDebug "Starting finisher"
---     liftIO $ Finisher.run env serverThreadId
---     logDebug "Bye"
